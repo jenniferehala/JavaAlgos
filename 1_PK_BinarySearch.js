164c7e31530df82b3368c1bede78c78ebdc4a593
jehala <jenniferehala@github.com>
2022-04-05 10:57:16 -0400

    commit

---
+++ 1_PK_BinarySearch.js
@@ -0,0 +1,25 @@
+// Binary Search
+
+//Write a function that given a sorted aray and a value, determines whther the calue is cfound within the array trhough
+//recursively searching and returns the index. Binary Search works by checking wither the value given is greater than or 
+// less than a midway point, which is why the given array must be sorted.
+//Also, even though there's only an array value given, you can add additional parameters to your funciton.
+//Return -1 if the number is not in the array
+
+function binarySearch(arr, value) {
+
+
+}
+
+// console.log(binarySearch([1,2], -1)) // -2
+// console.log(binarySearch([1,2], 4)) // 4
+// console.log(binarySearch([1,2], 1)) // 1
+// console.log(binarySearch([1,2,2], 2)) // 2
+// console.log(binarySearch([], 7)) // 7
+// console.log(binarySearch([1,2,2,2,2,2,2,4,5,5,5,5,6,6,6], 2)) // 2
+// console.log(binarySearch([1,2,3,3,4,4,5,6,7,8,8,9,10], 8)) // 2
+// console.log(binarySearch([1,1,1,1,1], 1)) // 1
+
+// make sure to test all given scenarios!
+//extra challengel don't use built-in function such as splice() or slice(),
+//with the exception of rounding functions (Math.dloor(), Math.ceil())
--- Practice/1_P_BalancePoint.js
+++ Practice/1_P_BalancePoint.js
@@ -2,32 +2,68 @@
 // Example [1,2,3,4,10] --> True
 // Balance point happens b/w indices 3&4 where the left side equals the right: [1,2,3,4 | 10 | ==> 1+2+3+4 = 10]
 
-// ***** this is Nick's Class soln **** I think
+// ***** this is Nick's Class soln **** Doesn't Work
 
-function balancePoint(arr) {
-    var sum = arr[0];
-    var arrSum = 0;
-    for (var i = arr.length - 1; i > 0; i--) {
-        arrSum += arr[i]
-        for (var j = 1; j < arr.length; j++) {
-            if (arr[2] == null) {
-                if (sum = arrSum) {
-                    return true
-                }
-            }
-            sum += arr[j]
-            if (sum === arrSum) {
-                return true
-            }
-        }
+// function balancePoint(arr) {
+//     var sum = arr[0];
+//     var arrSum = 0;
+//     for (var i = arr.length - 1; i > 0; i--) {
+//         arrSum += arr[i]
+//         for (var j = 1; j < arr.length; j++) {
+//             if (arr[2] == null) {
+//                 if (sum = arrSum) {
+//                     return true
+//                 }
+//             }
+//             sum += arr[j]
+//             if (sum === arrSum) {
+//                 return true
+//             }
+//         }
 
-    }
-    return false
-}
+//     }
+//     return false
+// }
+// console.log(balancePoint([4, 3, 7, 4, 10])); // TRUE
+// console.log(balancePoint([4, 3, 7, 4, 11, 2])); // FALSE
+// console.log(balancePoint([1, 2, 3, 1, 2, 3, 2, 1])); // FALSE
+// console.log(balancePoint([2, 2])); // TRUE
 // console.log(balancePoint([1, 2, 3, 4, 10])); // TRUE
 // console.log(balancePoint([1, 2, 3, 2, 1])); // FALSE
 // console.log(balancePoint([1, 2, 3, 1, 2, 3, 2, 1])); // FALSE
 // console.log(balancePoint([2, 2])); // TRUE
+
+
+// Diana's Soln - Works!
+
+function balancePoint(arr) {
+    var sum = 0
+    var leftSum = 0
+
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+
+    for (var i = 0; i < arr.length; i++) {
+        var rightSum = sum - leftSum - arr[i]
+        leftSum += arr[i]
+
+        if (leftSum === rightSum) {
+            return true;
+        }
+
+    }
+    return false;
+}
+
+console.log(balancePoint([4, 3, 7, 4, 10])); // TRUE
+console.log(balancePoint([4, 3, 7, 4, 11, 2])); // FALSE
+console.log(balancePoint([1, 2, 3, 1, 2, 3, 2, 1])); // FALSE
+console.log(balancePoint([2, 2])); // TRUE
+console.log(balancePoint([1, 2, 3, 4, 10])); // TRUE
+console.log(balancePoint([1, 2, 3, 2, 1])); // FALSE
+console.log(balancePoint([1, 2, 3, 1, 2, 3, 2, 1])); // FALSE
+console.log(balancePoint([2, 2])); // TRUE
 
 // **** Kasey's Solution
 
@@ -49,7 +85,9 @@ function balancePoint(arr) {
     return false
 }
 
-console.log(balancePoint([4, 3, 7, 4, 10])); // TRUE
-console.log(balancePoint([4, 3, 7, 4, 11, 2])); // FALSE
-console.log(balancePoint([1, 2, 3, 1, 2, 3, 2, 1])); // FALSE
-console.log(balancePoint([2, 2])); // TRUE
+// console.log(balancePoint([4, 3, 7, 4, 10])); // TRUE
+// console.log(balancePoint([4, 3, 7, 4, 11, 2])); // FALSE
+// console.log(balancePoint([1, 2, 3, 1, 2, 3, 2, 1])); // FALSE
+// console.log(balancePoint([2, 2])); // TRUE
+
+
